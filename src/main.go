5de74f03bded44307cc8d73c0687dbf286952f20
manupanand <manupanandindianinvestor@gmail.com>
2025-06-26 15:07:06 +0530

    updated for test in github

--- src/main.go
+++ src/main.go
@@ -5,14 +5,22 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/manupanand/01-fiber/model"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 
-	app := fiber.New()
 
+	app := fiber.New()
+    // ✅ Apply CORS middleware globally
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",   // here -origin-frontend Allow all origins - change to specific domains in production
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowCredentials: false, // for specific witthout wild card set -true
+	}))
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello world")
+		return c.SendString("This is manupanands testing api...")
 	})
 	app.Get("/:value", func(c *fiber.Ctx) error {
 		fmt.Println(model.Name)
@@ -20,6 +28,9 @@ func main() {
 		
 	})
 
-	app.Listen(":3000")
-	
+	// ✅ Start server
+	err := app.Listen(":3000")
+	if err != nil {
+		panic(err)
+	}
 }
